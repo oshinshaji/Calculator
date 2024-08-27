f07833184823e72c7f0bf5e66336298d5fb126bc
oshinshaji <oshinshaji501@gmail.com>
2024-08-27 12:11:25 +0530

    Initial commit

---
+++ calci.js
@@ -0,0 +1,13 @@
+function display(a) {
+  const k = document.getElementById('txt');
+  k.value +=  a;
+  }
+
+function solve(){
+  var n = document.getElementById('txt').value=eval(txt.value);
+  }
+function clr(){
+  document.getElementById('txt').value=" ";
+
+}
+  
